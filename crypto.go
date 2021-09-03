a58aea0434163178ccd8e8cb0623b90226a29490
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2021-09-03 20:36:33 +0100

    ..

--- crypto.go
+++ crypto.go
@@ -1,14 +1,35 @@
 package nlib
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	cRand "crypto/rand"
+	"encoding/hex"
 	"io"
 	"strings"
-	"crypto/aes"
-	"crypto/md5"
-	"crypto/cipher"
-	"encoding/hex"
-	cRand "crypto/rand"
 )
+
+func maxof2(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func minof2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(numbers ...int) int {
+	m := 0
+	for _, i := range numbers {
+		m = maxof2(m, i)
+	}
+	return m
+}
 
 func GetMD5Hash(text string) string {
 	hasher := md5.New()
@@ -16,8 +37,8 @@ func GetMD5Hash(text string) string {
 	return strings.ToUpper(hex.EncodeToString(hasher.Sum(nil)))
 }
 
-func Encrypt(data []byte) []byte {
-	block, _ := aes.NewCipher([]byte(GetMD5Hash("BiqLhan.2018!")))
+func Encrypt(data []byte, key []byte) []byte {
+	block, _ := aes.NewCipher(key)
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
@@ -30,8 +51,7 @@ func Encrypt(data []byte) []byte {
 	return ciphertext
 }
 
-func Decrypt(data []byte) []byte {
-	key := []byte(GetMD5Hash("BiqLhan.2018!"))
+func Decrypt(data []byte, key []byte) []byte {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
@@ -47,4 +67,59 @@ func Decrypt(data []byte) []byte {
 		panic(err.Error())
 	}
 	return plaintext
+}
+
+// XORBytes takes two byte slices and XORs them together, returning the final
+// byte slice.
+func XOR2Bytes(b1, b2 []byte) []byte {
+	oneAndTwo := make([]byte, max(len(b1), len(b2)))
+
+	for i := 0; i < len(b1) && i < len(b2); i++ {
+		oneAndTwo[i] = b1[i] ^ b2[i]
+	}
+	for i := len(b2); i < len(b1); i++ {
+		oneAndTwo[i] = b1[i]
+	}
+	for i := len(b1); i < len(b2); i++ {
+		oneAndTwo[i] = b2[i]
+	}
+	return oneAndTwo
+
+}
+
+// XORBytes takes two byte slices and XORs them together, returning the final
+// byte slice.
+func XORBytes(buffers ...[]byte) []byte {
+	maxLen := 0
+	for _, buff := range buffers {
+		maxLen = maxof2(maxLen, len(buff))
+	}
+	minLen := ^int(0)
+	for _, buff := range buffers {
+		minLen = minof2(minLen, len(buff))
+	}
+	result := make([]byte, maxLen)
+
+	for i := 0; i < minLen; i++ {
+		for bi, buff := range buffers {
+			if bi == 0 {
+				result[i] = buff[i]
+			} else {
+				result[i] = result[i] ^ buff[i]
+			}
+		}
+	}
+	for i := minLen - 1; i < maxLen; i++ {
+		for bi := 0; bi < len(buffers); bi++ {
+			if i < len(buffers[bi]) {
+				if result[i] == byte(0) {
+					result[i] = buffers[bi][i]
+				} else {
+					result[i] = result[i] ^ buffers[bi][i]
+				}
+			}
+		}
+
+	}
+	return result
 }
--- nset.go
+++ nset.go
@@ -1,36 +1,31 @@
 package nlib
 
 import (
-	//"os"
-	"encoding/gob"
-	//"io"
-	"fmt"
 	"bytes"
+	"encoding/gob"
 	"io/ioutil"
 	"reflect"
 )
 
-
-func SaveToNSF(filename string, obj interface{} ) {
+// SaveToNSF Encrypted bicimde Nuveus formatinda objeti kaydeder.
+func SaveToNSF(filename string, obj interface{}) {
 	bin := new(bytes.Buffer)
 	encoder := gob.NewEncoder(bin)
 	encoder.Encode(obj)
-	encoded:=Encrypt(bin.Bytes())
-	ioutil.WriteFile(filename,encoded,0644)
+	encoded := Encrypt(bin.Bytes(), []byte(GetMD5Hash("NSF.Object.2021")))
+	ioutil.WriteFile(filename, encoded, 0644)
 }
 
+// LoadFromNSF Encrypted bicimde Nuveus formatinda kaydedilmis objeyi doner
+func LoadFromNSF(filename string, o interface{}) interface{} {
 
-func LoadFromNSF(filename string, o interface {} ) interface {} {
+	v := reflect.New(reflect.TypeOf(o))
 
-	v:=reflect.New(reflect.TypeOf(o))
-
-	bin,_:=ioutil.ReadFile(filename)
-	dec:=Decrypt(bin)
-	buf:=new(bytes.Buffer)
+	bin, _ := ioutil.ReadFile(filename)
+	dec := Decrypt(bin, []byte(GetMD5Hash("NSF.Object.2021")))
+	buf := new(bytes.Buffer)
 	buf.Write(dec)
 	decoder := gob.NewDecoder(buf)
-	err:=decoder.Decode(v.Interface())
-	fmt.Printf("Val : %v\n ERR : %v",v,err)
+	decoder.Decode(v.Interface())
 	return v.Interface()
-
 }
